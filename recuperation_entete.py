32c4ef54f61071226bc76648da0c6dc3b09c7f13
fz3p <fzepparelli@protonmail.com>
2018-10-10 20:28:49 +0200

    conform python3

--- recuperation_entete.py
+++ recuperation_entete.py
@@ -1,11 +1,9 @@
 import csv
 import sys 
 
-
 # arg
 path_filename = sys.argv[1] 
 list_filename = sys.argv[2]
-
 file = open(list_filename, "w")
 
 # just first line 
@@ -13,17 +11,18 @@ line_number= 0
 diese = "# "
 with open(path_filename) as csvfile:
 	line = csv.reader(csvfile, delimiter=';')
- 	for row in line:
- 		column_number = 0
- 		if line_number == 0:
+	for row in line:
+		column_number = 0
+		if line_number == 0:
 	 		for cell in row:
 	 			file.write(diese + str(column_number) + " - " + cell + "\n")
 	 			# print(diese + str(numero_column) + " - " + cell)
 	 			column_number = column_number + 1
-	 	line_number = line_number + 1
+		line_number = line_number + 1
 file.close()
+
 
 # control at the end 
 file = open(list_filename, "r")
-print file.read()
+print(file.read())
 file.close()
