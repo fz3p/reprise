ee360098c1d2fe8b129538fbd809f4dc72a9847b
fz3p <fzepparelli@protonmail.com>
2018-10-13 13:41:13 +0200

    Add openbox

--- windows/windows.py
+++ windows/windows.py
@@ -1,7 +1,20 @@
 from tkinter import *
+from tkinter.filedialog import askopenfilename
+import os, os.path
+
+def set_filename():
+    file = askopenfilename()
+    file = file.name
+    filename.set(file)
+    print(filename)
+
 
 # windows
 windows = Tk()
+
+filename = StringVar(windows)
+label = Label(windows, text=filename)
+label.pack
 
 # title frame
 title = LabelFrame(windows, text="Voici la liste des colonnes de votre tableau")
@@ -10,6 +23,10 @@ title.pack(fill="both", expand="yes", padx=10, pady=10)
 # result
 result = Label(title, text="SEE RESULTS")
 result.pack()
+
+# openButton
+openButton = Button(windows, text='open', command=set_filename)
+openButton.pack()
 
 # quit button 
 quitButton = Button(windows)
