39fece29c01125f27e1a5e92579968f747461803
fz3p <fzepparelli@protonmail.com>
2018-10-14 13:59:32 +0200

    Add menuBar and show results

--- windows/windows.py
+++ windows/windows.py
@@ -7,35 +7,37 @@ from header_to_csv import header_to_csv
 def set_filename():
     file = askopenfilename()
     filename.set(file)
-    header_to_csv(filename)
+    array = header_to_csv(filename)
 
-
-
+    print(array)
+    for line in enumerate(array):
+        print(line)
+        Label(title, text=line).pack()
+        
 # windows
 windows = Tk()
-
+windows.geometry("400x500+300+0")
+windows.resizable(width=False,height=True)
 filename = StringVar(windows)
 label = Label(windows, text=filename)
 label.pack
 
-# title frame
-title = LabelFrame(windows, text="Voici la liste des colonnes de votre tableau")
-title.pack(fill="both", expand="yes", padx=10, pady=10)
-
-# result
-result = Label(title, text="SEE RESULTS")
-result.pack()
-
 # openButton
 FILETYPES = [ ("text files", "*.csv") ]
-openButton = Button(windows, text='open', command=set_filename)
-openButton.pack()
 
-# quit button 
-quitButton = Button(windows)
-quitButton['text'] = 'Quit'
-quitButton['command'] = windows.destroy
-quitButton.pack(side=RIGHT, padx=10, pady=10)
+# menubar
+menubar = Menu(windows)
+menu1 = Menu(menubar, tearoff=0)
+menu1.add_command(label="open", command=set_filename)
+menu1.add_separator()
+menu1.add_command(label="Quit", command=windows.destroy)
+menubar.add_cascade(label="File", menu=menu1)
+windows.config(menu=menubar)
+
+# title frame
+title = LabelFrame(windows, text="Column in array")
+title.pack(fill="both", expand="yes", padx=10, pady=10)
+
 
 windows.mainloop
 
